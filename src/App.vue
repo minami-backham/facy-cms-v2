d0a569e1711d85d3362fd499c49d6a84a0a35df7
MASAHIRO KUROKAWA <masahiro819@gmail.com>
2021-01-20 11:54:38 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Login />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld";
+import Login from "./components/Login";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld
+    Login,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-form v-model="valid" class="login">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="メールアドレス"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="password"
+            :rules="passwordRules"
+            :counter="10"
+            label="パスワード"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    // firstname: "",
+    // lastname: "",
+    // nameRules: [
+    //   (v) => !!v || "Name is required",
+    //   (v) => v.length <= 10 || "Name must be less than 10 characters",
+    // ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "メールアドレスの入力は必須です",
+      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    ],
+    password: "",
+    passwordRules: [
+      (v) => !!v || "パスワードの入力は必須です",
+      (v) => v.length <= 8 || "８文字以上の有効なパスワードを入力してください",
+    ],
+  }),
+};
+</script>
+
+<style>
+.login {
+  padding: 20px 10px;
+}
+</style>
