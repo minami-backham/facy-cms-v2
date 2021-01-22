aae844479f1e62dbfefd4fe0bf6e1cc4928612bb
minami-backham <57670024+minami-backham@users.noreply.github.com>
2021-01-22 21:51:47 +0900

    ルーター追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <v-container
-      ><v-row align="center"
-        ><v-col align-self="center"><div>オンライン相談</div></v-col
-        ><v-col align-self="center"><div>MAHOU MAKE CMS</div></v-col></v-row
-      ></v-container
-    >
+    <v-container>
+      <v-row align="center">
+        <!-- <v-col align-self="center"><div>オンライン相談</div></v-col> -->
+        <!-- <v-col align-self="center"><div>MAHOU MAKE CMS</div></v-col> -->
+      </v-row>
+      <div>
+        <div>各画面へのリンク</div>
+        <v-btn to="/login">ログイン画面</v-btn>
+        <v-btn to="/reset">パスワードリセット画面</v-btn>
+        <v-btn to="/schedule">スケジュール画面</v-btn>
+      </div>
+    </v-container>
   </div>
 </template>
 <script>
