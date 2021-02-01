b943578b837f4f6c08d3a3252978ea7134f2f38a
iida <iida.backham@gmail.com>
2021-02-01 14:06:50 +0900

    add components
    week, dateコンポーネント配置

--- src/views/ManageTable.vue
+++ src/views/ManageTable.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="schedule">
-    <ManageTableWeek />
+    <v-btn to="">保存</v-btn>
+    <!-- <ManageTableWeek :configData="configData.day_of_week" /> -->
+    <ManageTableDate :configData="configData" />
   </div>
 </template>
 
@@ -8,12 +10,23 @@
 import * as _ from "lodash";
 // import manageTimetableHeader from "../components/manageTimetableHeader.vue";
 import ManageTableWeek from "../components/ManageTableWeek.vue";
+import ManageTableDate from "../components/ManageTableDate.vue";
+import CONFIG_SCHEDULE from "../../public/json/config_schedule.json";
 
 export default {
   name: "managetable",
   components: {
     // manageTimetableHeader,
     ManageTableWeek,
+    ManageTableDate,
+  },
+  data() {
+    return {
+      configData: CONFIG_SCHEDULE,
+    };
+  },
+  mounted() {
+    // this.configData = CONFIG_SCHEDULE;
   },
 };
-</script> 
+</script>
