4c9755e5de3350edf38079e032ef93c42125fa6b
iida <iida.backham@gmail.com>
2021-02-24 15:35:58 +0900

    api update

--- src/api/statics.js
+++ src/api/statics.js
@@ -1,11 +1,11 @@
 const DAY_OF_WEEK = {
-  MONDAY: { weekNum: 2, id: "Monday", label: "月曜日", shortLabel: "月" },
-  TUESDAY: { weekNum: 3, id: "Tuesday", label: "火曜日", shortLabel: "火" },
-  WEDNESDAY: { weekNum: 4, id: "Wednesday", label: "水曜日", shortLabel: "水" },
-  THURSDAY: { weekNum: 5, id: "Thursday", label: "木曜日", shortLabel: "木" },
-  FRIDAY: { weekNum: 6, id: "Friday", label: "金曜日", shortLabel: "金" },
-  SATURDAY: { weekNum: 7, id: "Saturday", label: "土曜日", shortLabel: "土" },
-  SUNDAY: { weekNum: 1, id: "Sunday", label: "日曜日", shortLabel: "日" }
+  MONDAY: { id: "Monday", label: "月曜日", shortLabel: "月", index: 1 },
+  TUESDAY: { id: "Tuesday", label: "火曜日", shortLabel: "火", index: 2 },
+  WEDNESDAY: { id: "Wednesday", label: "水曜日", shortLabel: "水", index: 3 },
+  THURSDAY: { id: "Thursday", label: "木曜日", shortLabel: "木", index: 4 },
+  FRIDAY: { id: "Friday", label: "金曜日", shortLabel: "金", index: 5 },
+  SATURDAY: { id: "Saturday", label: "土曜日", shortLabel: "土", index: 6 },
+  SUNDAY: { id: "Sunday", label: "日曜日", shortLabel: "日", index: 0 },
 };
 
 export { DAY_OF_WEEK };
