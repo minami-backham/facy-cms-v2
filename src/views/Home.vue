f6cb321854bb9e528671d1028b370adff53f4130
iida <iida.backham@gmail.com>
2021-02-24 18:01:05 +0900

    login path fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <v-container class="home">
     <v-row class="px-4">各画面へのリンク</v-row>
     <v-row class="mt-4 px-4">
-      <v-btn class="mr-4 mt-4" to="/login">ログイン画面</v-btn>
+      <v-btn class="mr-4 mt-4" to="/manage/login">ログイン画面</v-btn>
       <v-btn class="mr-4 mt-4" to="/manage/reset">パスワードリセット画面</v-btn>
       <v-btn class="mr-4 mt-4" to="/manage/schedule">スケジュール画面</v-btn>
       <v-btn class="mr-4 mt-4" to="/manage/managetable">予約可能日設定</v-btn>
@@ -11,7 +11,7 @@
   </v-container>
 </template>
 <script>
-  export default {
-    name: "home",
-  };
+export default {
+  name: "home",
+};
 </script>
